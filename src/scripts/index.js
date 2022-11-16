77c1186294b359626996f977011ebbf46f42f5e7
Giovanni Perotto <giovanniperotto@hotmail.com>
2022-11-16 20:03:00 -0300

    feat: create css styles

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+document.querySelector('#address').addEventListener('click', () => {
+  console.log('oi')
+  //window.open('https://google.com', '_blank')
+  //window.open('mailto:nowhere@mozilla.org', '_blank')
+  window.open('https://wa.me/5541999999999?text=Olá, vi seu site e estou entrando em contato pois gostaria de realizar um orçamento.', '_blank')
+})
+
+//link to read
+//https://brightwhiz.com/send-whatsapp-message-through-html-link/
