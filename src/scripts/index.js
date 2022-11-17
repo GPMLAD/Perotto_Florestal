ff1538e6ce7a17cceeeec7ec5625a5ec93b0564d
Giovanni Perotto <giovanniperotto@hotmail.com>
2022-11-16 21:02:51 -0300

    feat: create footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,10 @@ document.querySelector('#address').addEventListener('click', () => {
   console.log('oi')
   //window.open('https://google.com', '_blank')
   //window.open('mailto:nowhere@mozilla.org', '_blank')
-  window.open('https://wa.me/5541999999999?text=Olá, vi seu site e estou entrando em contato pois gostaria de realizar um orçamento.', '_blank')
+  window.open(
+    'https://wa.me/5541999999999?text=Olá, vi seu site e estou entrando em contato para mais informações, você poderia me ajudar?',
+    '_blank'
+  )
 })
 
 //link to read
